aa5b0c9cc4244edac09f3b5cb602a9ec5d41df34
AlexZ1937 <alexz20937cany3@gmail.com>
2020-07-09 18:20:10 +0300

    Framework+repository

---
+++ AlexSTOv2/AlexSTOv2/Models/Repository/EDBContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace AlexSTOv2.Models.Repository
+{
+    public class EFDBContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+
+    }
+}
---
+++ AlexSTOv2/AlexSTOv2/Models/Repository/Repository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlexSTOv2.Models.Repository
+{
+    public class Repository
+    {
+        private EFDBContext context = new EFDBContext();
+        public IEnumerable<Client> Clients { get { return context.Clients; } }
+    }
+}
