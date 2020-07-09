d53aabdd217f53b1a5006ea5d0ff4d2b156ad7c0
AlexZ1937 <alexz20937cany3@gmail.com>
2020-07-09 18:24:32 +0300

    DBSet EDBContext

--- AlexSTOv2/AlexSTOv2/Models/Repository/EDBContext.cs
+++ AlexSTOv2/AlexSTOv2/Models/Repository/EDBContext.cs
@@ -6,5 +6,19 @@ namespace AlexSTOv2.Models.Repository
     {
         public DbSet<Client> Clients { get; set; }
 
+        public DbSet<Service> Services { get; set; }
+
+        public DbSet<Good> Goods { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Car> Cars { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Master> Masters { get; set; }
+
+        public DbSet<Cart> Carts { get; set; }
+
     }
 }
