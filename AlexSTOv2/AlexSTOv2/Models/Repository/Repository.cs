7e971465a98c01a52ee07347ab029daee9d066ac
AlexZ1937 <alexz20937cany3@gmail.com>
2020-07-09 18:28:06 +0300

    Repository iEnumerables

--- AlexSTOv2/AlexSTOv2/Models/Repository/Repository.cs
+++ AlexSTOv2/AlexSTOv2/Models/Repository/Repository.cs
@@ -9,5 +9,15 @@ namespace AlexSTOv2.Models.Repository
     {
         private EFDBContext context = new EFDBContext();
         public IEnumerable<Client> Clients { get { return context.Clients; } }
+
+        public IEnumerable<Good> Goods { get { return context.Goods; } }
+
+        public IEnumerable<Master> Masters { get { return context.Masters; } }
+
+        public IEnumerable<Service> Services { get { return context.Services; } }
+
+        public IEnumerable<Car> Cars { get { return context.Cars; } }
+
+    
     }
 }
